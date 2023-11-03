fbc24c5f18c89c4ddd8074430ec5dccff0a53262
Sudhanshu-110 <130098416+Sudhanshu-110@users.noreply.github.com>
2023-11-03 10:22:41 +0530

    Add files via upload

---
+++ pythonproj.py
@@ -0,0 +1,13 @@
+num =int(input("Enter the number to be checked"))
+if num > 1:
+    
+    for i in range(2, int(num/2)+1):
+        # If num is divisible by any number between
+        # 2 and n / 2, it is not prime
+        if (num % i) == 0:
+            print(num, "is not a prime number")
+            break
+    else:
+        print(num, "is a prime number")
+else:
+    print(num, "is not a prime number")
